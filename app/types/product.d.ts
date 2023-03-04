5580ad5f4ab3890ca75b4c6ee88247cea04bfb60
Saeed Badran <salouri@gmail.com>
2023-03-04 17:56:46 +0400

    modify typescript product types

--- app/types/product.d.ts
+++ app/types/product.d.ts
@@ -1,14 +1,18 @@
 type ProductType = {
+  _id: any;
   name: string;
   description: string;
   price: number;
-  soldout: boolean;
+  soldout: SoldoutType;
   stores: StoreType[];
+  inventory: number;
 };
+
+type SoldoutType = 'SOLDOUT' | 'ONSTOCK';
 
 type StoreType = {
   id: string;
   name: String;
 };
 
-export { ProductType, StoreType };
+export { ProductType, StoreType, SoldoutType };
